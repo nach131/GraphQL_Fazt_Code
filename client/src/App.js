c193b7ae6c0bbd72cb59ec37a11a8f231404ad37
nacho <nacho.mota@gmail.com>
2020-09-22 15:55:45 +0200

    Inicio

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import MessageList from './components/MessageList'
-import MessageForm from './components/MesageForm'
+// import MessageList from './components/MessageList'
+// import MessageForm from './components/MesageForm'
 import { Navigator } from './components/Navbar';
 
-import 'bootswatch/dist/superhero/bootstrap.min.css'
+import 'bootswatch/dist/sketchy/bootstrap.min.css'
 
 function App() {
   return (
@@ -12,8 +12,9 @@ function App() {
  <Navigator/>
  <div className="container p-4">
  <Switch>
-     <Route exact path="/" component={MessageList}/>
-     <Route exact path="/new-message" component={MessageForm}/>
+     {/* <Route exact path="/" component={MessageList}/> */}
+     {/* <Route exact path="/new-message" component={MessageForm}/> */}
+     <h1>App</h1>
    </Switch>
  </div>
  </Router>
